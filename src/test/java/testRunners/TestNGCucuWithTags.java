826b41a8cc0945d89e5cae5ebe831f2517c8b20e
Jeevan <jeenulingappa@gmail.com>
2020-06-11 14:02:20 +0530

    Adding tags new testrunner file

---
+++ src/test/java/testRunners/TestNGCucuWithTags.java
@@ -0,0 +1,21 @@
+package testRunners;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		features="src/test/java/features",
+		glue="stepDefinations",
+		tags= {"@RadioButton"},
+		plugin= {"pretty","timeline:target/Timeline"
+				,"html:target/HTMLReport"
+				,"json:target/jsonRport/report.json"
+				,"junit:target/jUnitReport/JReport.xml"},
+		monochrome=true
+		)
+public class TestNGCucuWithTags extends AbstractTestNGCucumberTests {
+	
+
+}
--- target/Timeline/report.js
+++ target/Timeline/report.js
@@ -1,81 +1,21 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "29e67880-3003-4d9f-bd7a-2d700488d253",
+    "id": "6e3b8024-5ab6-4941-82ff-fdd6dd05d271",
     "feature": "Validation of Radio Buttons demo on seleniumEasy website",
     "scenario": "Single Radio button demo",
-    "start": 1591862907443,
-    "end": 1591862926053,
-    "group": 19,
+    "start": 1591864240083,
+    "end": 1591864252693,
+    "group": 1,
     "content": "",
     "className": "passed",
     "tags": "@radiobutton,@parallel,"
-  },
-  {
-    "id": "bb482c3b-d085-49ac-b9b7-9ac51580358d",
-    "feature": "Validation of Check Box demo on seleniumEasy website",
-    "scenario": "Multiple Check box demo UnCheck All",
-    "start": 1591862907443,
-    "end": 1591862926720,
-    "group": 17,
-    "content": "",
-    "className": "passed",
-    "tags": "@checkbox,@multicheckbox,"
-  },
-  {
-    "id": "2f4103f9-0721-4888-9deb-846a399f41f8",
-    "feature": "Validation of Check Box demo on seleniumEasy website",
-    "scenario": "Multiple Check box demo UnCheck All with check all button validation",
-    "start": 1591862907444,
-    "end": 1591862924242,
-    "group": 18,
-    "content": "",
-    "className": "passed",
-    "tags": "@checkbox,@multicheckbox,"
-  },
-  {
-    "id": "a86210c7-7cf5-45b1-a523-6d5770f73c7a",
-    "feature": "Validation of Check Box demo on seleniumEasy website",
-    "scenario": "Multiple Check box demo CheckAll",
-    "start": 1591862907443,
-    "end": 1591862926336,
-    "group": 16,
-    "content": "",
-    "className": "passed",
-    "tags": "@checkbox,@multicheckbox,"
-  },
-  {
-    "id": "d035eeac-67f4-4248-a79b-06c6dbe289e8",
-    "feature": "Validation of Check Box demo on seleniumEasy website",
-    "scenario": "Single check box demo",
-    "start": 1591862907443,
-    "end": 1591862923613,
-    "group": 15,
-    "content": "",
-    "className": "passed",
-    "tags": "@checkbox,@multicheckbox,@parallel,"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
-    "id": 15,
-    "content": "Thread[TestNG-PoolService-0,5,main]"
-  },
-  {
-    "id": 16,
-    "content": "Thread[TestNG-PoolService-1,5,main]"
-  },
-  {
-    "id": 17,
-    "content": "Thread[TestNG-PoolService-2,5,main]"
-  },
-  {
-    "id": 18,
-    "content": "Thread[TestNG-PoolService-3,5,main]"
-  },
-  {
-    "id": 19,
-    "content": "Thread[TestNG-PoolService-4,5,main]"
+    "id": 1,
+    "content": "Thread[main,5,main]"
   }
 ]);
 });
